8a74e93beb9325eccf4a5763f2ec6704b80bac8d
Vitor Eduardo Silva Dias <vitor.ramone2703@gmail.com>
2022-10-19 10:29:39 +0100

    first commit simple calculator structure

---
+++ simple_calculator.py
@@ -0,0 +1,108 @@
+from tkinter import*
+
+root = Tk()
+root.title('simple calculator app')
+#input and output
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+symbol = 0
+
+#functions dos buttons 
+def Button_click(number):
+    #e.delete(0,END)
+    current = e.get()
+    e.delete(0,END)
+    e.insert(0, str(current) + str(number))
+
+def Button_clear():
+    e.delete(0,END)
+
+def Button_equal():
+    second_number = e.get()
+    e.delete(0,END)
+    if  Maths_Symbols == 'add':
+        e.insert(0,f_number + int(second_number))
+    elif Maths_Symbols == 'sub':
+        e.insert(0,f_number - int(second_number))
+    elif Maths_Symbols == 'mul':
+        e.insert(0,f_number * int(second_number))
+    elif Maths_Symbols == 'div':
+        e.insert(0,f_number / int(second_number))
+
+
+def First_number(first_number):
+    global f_number
+    f_number = int(first_number)
+    e.delete(0,END)
+
+def Button_add():
+    global  Maths_Symbols
+    Maths_Symbols = 'add'
+
+    first_number = e.get()
+    First_number(first_number)
+    
+def Button_subtract():
+    global  Maths_Symbols
+    Maths_Symbols = 'sub'
+
+    first_number = e.get()
+    First_number(first_number)
+
+def Button_multiply():
+    global  Maths_Symbols
+    Maths_Symbols = 'mul'
+
+    first_number = e.get()
+    First_number(first_number)
+
+def Button_divide():
+    global  Maths_Symbols
+    Maths_Symbols = 'div'
+
+    first_number = e.get()
+    First_number(first_number)
+
+#creat the buttons
+button_1 = Button(root, text='1', padx=40, pady=20, command=lambda: Button_click(1))
+button_2 = Button(root, text='2', padx=40, pady=20, command=lambda: Button_click(2))
+button_3 = Button(root, text='3', padx=40, pady=20, command=lambda: Button_click(3))
+button_4 = Button(root, text='4', padx=40, pady=20, command=lambda: Button_click(4))
+button_5 = Button(root, text='5', padx=40, pady=20, command=lambda: Button_click(5))
+button_6 = Button(root, text='6', padx=40, pady=20, command=lambda: Button_click(6))
+button_7 = Button(root, text='7', padx=40, pady=20, command=lambda: Button_click(7))
+button_8 = Button(root, text='8', padx=40, pady=20, command=lambda: Button_click(8))
+button_9 = Button(root, text='9', padx=40, pady=20, command=lambda: Button_click(9))
+button_0 = Button(root, text='0', padx=40, pady=20, command=lambda: Button_click(0))
+button_add = Button(root, text='+', padx=39, pady=20, command= Button_add)
+button_equal = Button(root, text='=', padx=87, pady=20, command= Button_equal)
+button_clear = Button(root, text='CLEAR', padx=73, pady=20, command= Button_clear)
+button_subtract = Button(root, text='-',padx=40, pady=20 , command= Button_subtract)
+button_multiply = Button(root, text ='*',padx=40, pady=20 , command= Button_multiply)
+button_divide = Button(root, text='/',padx=40, pady=20 , command= Button_divide)
+
+#layer and collun dos buttons 
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=1)
+button_add.grid(row=4, column=0)
+button_subtract.grid(row=4, column=2)
+
+button_clear.grid(row=5, column=1, columnspan=2)
+button_equal.grid(row=6, column=1, columnspan=2)
+
+
+button_multiply.grid(row=5, column=0)
+button_divide.grid(row=6, column=0)
+root.mainloop()
